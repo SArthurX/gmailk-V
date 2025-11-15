5337fa09e36be97c0af397de5409e58b4a09c92b
SArthurX <sxa05z@gmail.com>
2025-11-15 19:20:57 +0800

    btn test

--- wiringx-blink/blink.c
+++ wiringx-blink/blink.c
@@ -1,34 +1,50 @@
 #include <stdio.h>
 #include <unistd.h>
-
 #include <wiringx.h>
 
 int main() {
-    // Duo/Duo256M: LED = 25
-    // DuoS:        LED =  0
-    int DUO_LED = 0;
+    int LED = 25;   
+    int BTN = 21;   
 
-    // Duo:     milkv_duo
-    // Duo256M: milkv_duo256m
-    // DuoS:    milkv_duos
-    if(wiringXSetup("milkv_duos", NULL) == -1) {
+    if(wiringXSetup("milkv_duo256m", NULL) == -1) {
+        printf("Failed to init wiringX\n");
         wiringXGC();
         return 1;
     }
 
-    if(wiringXValidGPIO(DUO_LED) != 0) {
-        printf("Invalid GPIO %d\n", DUO_LED);
+    // GPIO 模式設定
+    pinMode(LED, PINMODE_OUTPUT);  
+    pinMode(BTN, PINMODE_INPUT);    
+    if(wiringXValidGPIO(LED) != 0) {
+        printf("Invalid GPIO %d\n", LED);
+    }
+    if(wiringXValidGPIO(BTN) != 0) {
+        printf("Invalid GPIO %d\n", BTN);
     }
 
-    pinMode(DUO_LED, PINMODE_OUTPUT);
+    printf("Press the button to toggle LED (BTN=21, LED=25)\n");
 
-    while(1) {
-        printf("Duo LED GPIO (wiringX) %d: High\n", DUO_LED);
-        digitalWrite(DUO_LED, HIGH);
-        sleep(1);
-        printf("Duo LED GPIO (wiringX) %d: Low\n", DUO_LED);
-        digitalWrite(DUO_LED, LOW);
-        sleep(1);
+    int led_state = 0;     
+    int last_state = LOW; 
+    int current_state;
+    digitalWrite(LED, LOW);
+
+    while (1) {
+        current_state = digitalRead(BTN);
+
+        if (last_state == HIGH && current_state == LOW) {
+            usleep(30000);             
+            if (digitalRead(BTN) == LOW) {
+                led_state = !led_state;
+                digitalWrite(LED, led_state ? HIGH : LOW);
+
+                printf("Button pressed → LED = %s\n",
+                       led_state ? "ON" : "OFF");
+            }
+        }
+
+        last_state = current_state;
+        usleep(1000);  
     }
 
     return 0;
